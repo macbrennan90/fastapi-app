5f38e3c8eab199fc602d7559d7f206be1d97ca78
macbrennan90 <mac.brennan.90@gmail.com>
2021-04-27 12:21:30 -0400

    save place

--- project/app/main.py
+++ project/app/main.py
@@ -5,10 +5,16 @@ from app.config import Settings, get_settings
 app = FastAPI()
 
 
+@app.get("/")
+async def base():
+    return "hello world the app has been updated"
+
+
 @app.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
     return {
         "ping": "pong!",
         "environment": settings.environment,
         "testing": settings.testing,
+        "update": "The app has been updated"
     }
